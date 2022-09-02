d7caa6d622497c6f428377c0b106299644607598
imsidz <imsidzluv@gmail.com>
2022-09-02 15:41:31 +0530

    upgraded to laravel

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ExampleController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function index()
     {
         return view('test');
     }
+
 }
